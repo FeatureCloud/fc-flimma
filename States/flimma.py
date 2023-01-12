d99ec2df2b7dcb0dd12223d8972bde04b8523c4d
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2023-01-12 14:37:34 +0100

    add licence

--- States/flimma.py
+++ States/flimma.py
@@ -1,3 +1,16 @@
+"""
+    FeatureCloud Flimma Application
+    Copyright 2023 Mohammad Bakhtiari. All Rights Reserved.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
 from FeatureCloud.app.engine.app import Role, AppState, app_state
 from .InitStates import LocalMean, GlobalMean
 from .cpmcutoff import CPMCutOff, CutOffAggregation
