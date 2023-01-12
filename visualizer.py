b12fa158eb651d78eba419f7c36e8d089d767d12
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2023-01-12 14:37:25 +0100

    rm extra tabs. and document

--- visualizer.py
+++ visualizer.py
@@ -1,9 +1,23 @@
+"""
+    FeatureCloud Flimma Application
+    Copyright 2023 Mohammad Bakhtiari. All Rights Reserved.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
 import os
-from dash import Dash, dcc, html
+from dash import dcc
 import dash_bio
 import numpy as np
 import math
-from time import sleep
+from time import sleep, time
 from threading import Thread
 
 import fcvisualization
@@ -22,10 +36,6 @@ def callback_fn_terminal_state():
 
 
 def plot_volcano(df):
-    print(df.shape)
-    print(df.columns)
-    print(df.index.values[:5])
-    print(df.head(1))
     global fc_visualization, fig, fig2
     path_prefix_visualizer = os.getenv("PATH_PREFIX") + 'visualizer/'
     print("PATH_PREFIX environment variable: ", path_prefix_visualizer)
@@ -35,8 +45,6 @@ def plot_volcano(df):
     tab_children = [
         dcc.Tab(label='Volcano plot', value='tab-volcano-plot', style=volcano_plot_style),
     ]
-    tab_value = 'tab-volcano-plot'
-    show_toast = False
     min_effect = math.floor(df['EFFECTSIZE'].min())
     max_effect = math.ceil(df['EFFECTSIZE'].max())
     min_effect_value = math.floor(min_effect + 0.3 * (max_effect - min_effect))
@@ -56,51 +64,38 @@ def plot_volcano(df):
         "title": "Volcano",
         "fig": fig,
     }]
-    print('Plot start...')
+    print_help_msg()
     # We start the visualization in a thread
     thread_vis = Thread(target=fc_visualization.start, args=(
         'fc', path_prefix_visualizer, callback_fn_terminal_state, extra_visualization_content))
     thread_vis.start()
 
-    print('Plot start...')
-    # We start the visualization in a thread
-    thread_vis = Thread(target=fc_visualization.start,
-                        args=('fc', path_prefix_visualizer, callback_fn_terminal_state, extra_visualization_content))
-    thread_vis.start()
+    # # We start the visualization in a thread
+    # thread_vis = Thread(target=fc_visualization.start,
+    #                     args=('fc', path_prefix_visualizer, callback_fn_terminal_state, extra_visualization_content))
+    # thread_vis.start()
     wait_for_termination()
 
 
-def wait_for_termination():
-    while (True):
+def wait_for_termination(last_print=time()):
+    while True:
         global vis_objects, fig, fig2
         # When the callback function will fire in the visualizer app, it'll trigger Finished state
         if TERMINAL is True:
             print('plot is finished')
             break
 
-        original_title = "My Diagram from State machine"
-        if len(vis_objects) == 2:
-            # Update the diagram added in the previous iteration
-            for diagram in vis_objects:
-                if diagram['title'] == original_title:
-                    print('Update diagram in progress')
-                    diagram['title'] = original_title + ' updated'
-                    diagram['fig'] = fig2
-                    vis_objects = fc_visualization.update_diagram(diagram)
-            # Add a one more diagram
-            vis_objects = fc_visualization.add_diagram([{
-                "title": "My second diagram from state machine",
-                "fig": fig2,
-            }])
-
-        if len(vis_objects) == 0:
-            # Add a new diagram to the UI
-            print("Adding a new diagram to the UI")
-            vis_objects = fc_visualization.add_diagram([{
-                "title": original_title,
-                "fig": fig,
-            }])
-
-        print(f'Visualization objects ==> {vis_objects}')
-        print('plot is running')
+        if time() > last_print + 10:
+            print_help_msg()
+            last_print = time()
         sleep(2)
+
+
+def print_help_msg():
+    print("The volcano plot is available in the app's front-end. You can have access to the plot by taking the "
+          "following steps:\n "
+          "\t1. Click on the front-end button.\n"
+          "\t2. Click on the `View in the new tab` button.\n"
+          "\t3. In the new tab, add `/visualizer` to the URL and press Enter.\n"
+          "\t4. Go to the `Volcano` tab to view the plot.\n"
+          "To finish the app execution, please click on the `Finish` button at the right corner.")
